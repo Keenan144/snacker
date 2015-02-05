b7b0d43f051d167adc4eccbe430e14bdec30d3ce
Keenan Sturtevant & vibrantlife <pair+keenan144.vibrantlife@devbootcamp.com>
2015-02-05 12:13:08 -0800

    routes are in progress and working, index, snack, and homepage views are started

---
+++ app/controllers/index.rb
@@ -0,0 +1,42 @@
+get '/' do
+  # goes to view /index.erb
+  @snacks = Tweet.all
+  erb :index
+end
+
+get '/snack/new' do
+#return html form to create a new tweet
+  erb :new_snack
+end
+
+post '/snack' do
+  #create a new snack
+  @snack = Tweet.create(params[:data])
+  redirect '/'
+end
+
+get '/snack/:id' do |id|
+  #display a specific snack
+
+end
+
+get '/snack/:id/edit' do
+  #return html form for editing snack
+end
+
+put '/snack/:id' do
+  #updates a specific snack
+  redirect '/'
+end
+
+delete '/snack/:id' do
+  #delete a snack
+  redirect '/'
+end
+
+get '/homepage' do
+
+  erb :homepage
+end
+
+
