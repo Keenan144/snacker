b23caf1664d190a9f225196ce125532154530ff2
Cordelia Peters & Andrew Yan <pair+cordeliapeters.yandrew@devbootcamp.com>
2015-02-06 15:52:34 -0800

    fixed nav buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,4 @@ User.create(handle: "imabigcordealia", password: "hello", name: "Cordelia Peters
 User.create(handle: "yandrew", password: "12345", name: "Andrew Yan")
 
 
-@counter = 3
-
-3.times do |tweet|
-  @counter += 1
-  Tweet.create(content: "Who runs the world!", user_id: @counter)
-end
 
