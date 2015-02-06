1185dfcda6e308d558fa53386dfb930e23236fc7
Cordelia Peters & Andrew Yan <pair+cordeliapeters.yandrew@devbootcamp.com>
2015-02-06 15:30:16 -0800

    working

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,10 @@ User.create(handle: "imabigcordealia", password: "hello", name: "Cordelia Peters
 User.create(handle: "yandrew", password: "12345", name: "Andrew Yan")
 
 
-@counter = 3
+# @counter = 3
 
-3.times do |tweet|
-  @counter += 1
-  Tweet.create(content: "Who runs the world!", user_id: @counter)
-end
+# 3.times do |tweet|
+#   @counter += 1
+#   Tweet.create(content: "Who runs the world!", user_id: @counter)
+# end
 
